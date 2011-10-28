c0e131e7be1a72a6af4c7257888a7ec4a56da04a
Tom Xie <tom.xie@nebutown.com>
2011-10-28 09:57:22 +0800

    modify the fork test to wait for the child

--- questions/time/fork.c
+++ questions/time/fork.c
@@ -6,11 +6,16 @@ extern void* func(void* args);
 
 int main()
 {
-    if (0 == fork())
+    pid_t pid = fork();
+    if (0 == pid)
     {
         func(NULL);
         return 0;
     }
+
     func(NULL);
+
+    wait(pid);
+
     return 0;
 }
