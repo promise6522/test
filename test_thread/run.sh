e1cdd991db641b28fa1e51f5dc38d4040af9f6cf
Tom Xie <tom.xie@nebutown.com>
2011-11-23 12:42:09 +0800

    update a test for g++ local statics

--- test_thread/run.sh
+++ test_thread/run.sh
@@ -1,3 +1,5 @@
 #! /bin/bash
-echo "Compiling..."
-g++ test.cpp -lpthread -g
+#echo "Compiling..."
+#g++ test.cpp -lpthread -g
+g++ singleton.cpp -lpthread -o ./bin/singleton_safe
+g++ singleton.cpp -lpthread -fno-threadsafe-statics -o ./bin/singleton_unsafe
